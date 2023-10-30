a58343acabe4859e3e887f714cd7b99b06822da8
mrilia8911 <148093177+mrilia8911@users.noreply.github.com>
2023-10-30 20:19:19 +0330

    Update bakery.py

--- bakery.py
+++ bakery.py
@@ -1,74 +1,71 @@
-#saeed bakery in python plus class
-total_1 = 0
-total_2 = 0
-total_3 = 0     #this is add some one
-total_4 = 0
-total_5 = 0
+print("barbary = 1$")
+print("lavash = 2$")
+print("sangak = 3$")
+print("bugget = 4$")
+print("konjedi = 5$")
+one = 0
+two = 0
+three = 0
+four = 0
+five = 0
 
-breads = {
-    "barbari": 1000,
-    "sangak" : 3000,
-    "lavash" : 500,     #this is dict
-    "taftoon": 2500,
-    "shirmal": 1750
-}
+breadRoom ={
+"barbary":1,
+"lavash":2,
+"sangak":3,
+"bugget":4,
+"konjedi":5
+    }
 
-print ("welcame to saeed bakery")
-print ("Address: 22 ghaem shahr BLVD")
+print("do you want breads? yes or no?")
+yn = input()
+while yn == "yes" :
+    print("wich?")
+    wich = input()
+    if wich == "barbary" :
+        print("how many?")
+        one = int(input())
+        one = one*1
+        print("do you want to buy more? yes or no?")
+        yn = input()
+    elif wich == "lavash" :
+         print("how many?")
+         two = int(input())
+         two = two*2
+         print("do you want to buy more? yes or no?")
+         yn = input()
+    elif wich == "sangak" :
+             print("how many?")
+             three = int(input())
+             three = three*3
+             print("do you want to buy more? yes or no?")
+             yn = input()
+    elif wich == "bugget" :
+        print("how many?")
+        four = int(input())
+        four = four*4
+        print("do you want to buy more? yes or no?")
+        yn = input()
+        
+    elif wich == "sangak" :
+        print("how many?")
+        five = int(input())
+        five = five*5
+        print("do you want to buy more? yes or no?")
+        yn = input()
+        
+    else :
+        print("ok good bye")
 
-switch = 0
-name = input ("please enter your name: ")
-phone_number = input ("please enter your phone number: ")
-card_number = input ("please insert your card: ")
+print("your bill =",one+two+three+four+five,"$")
+    
+    
+       
+    
 
-password_1 = input ("please create your password: ")
-password_2 = input ("please enter your password: ")
-self_checkout = "notdefined"
-##############start for password #######################
-for password in range(2):
-    while password_1 != password_2:
-        print("wrong password")
-        print("try again")
-        password_2 = input("plz enter your password")
-print ("your password is accepted")
 
-while self_checkout != "done":
-    if switch == 0:
-        self_checkout = input(f"{name} enter your bread: ")
-    else:
-        self_checkout = input("please enter your next bread")
-    while self_checkout not in dict(breads):
-        if self_checkout == "done":
-            break
-        self_checkout = input("please enter exsisting bread: ")
-    if self_checkout == "done":
-        break
-    customer = int (input(f"how many {self_checkout} did you ger? "))
+    
 
-    if self_checkout == "barbari":
-        total_1 = breads["barbari"] * customer + total_1
-        print (f"total is {total_1}")
 
-    elif self_checkout == "sangak":
-        total_2 = breads["sangak"] * customer + total_2
-        print (f"total is {total_2}")
-
-    elif self_checkout == "lavash":
-        total_3 = breads["lavash"] * customer + total_3
-        print (f"total is {total_3}")
-
-    elif self_checkout == "taftoon":
-        total_4 = breads["taftoon"] * customer + total_4
-        print (f"total is {total_4}")
-
-    elif self_checkout == "shirmal":
-        total_5 = breads["shirmal"] * customer + total_5
-        print (f"total is {total_5}")
-
-    total = total_1 + total_2 + total_3 + total_4 + total_5
-
-    print ("your bread had been enterd")
-    switch=1
-
-print (f"so your bill is {total}")
-#the end
+              
+ 
